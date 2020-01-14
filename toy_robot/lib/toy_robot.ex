9fde7f3d1413d9cda37746ed5b91aab74aa0404e
Orion Engleton <rsgrafx@gmail.com>
2020-01-14 22:43:06 +1200

    Returned list of points - adhear to visual grid

--- toy_robot/lib/toy_robot.ex
+++ toy_robot/lib/toy_robot.ex
@@ -1,8 +1,28 @@
 defmodule ToyRobot do
   @doc "Generate base table - grid"
-  def table do
-    for x <- 0..4,
-        y <- 0..4,
-        do: [x, y]
+
+  def moves() do
+    [:n, :s, :e, :w]
+  end
+
+  @doc """
+  Builds table grid with 0,0 at S/W and 4,$ at NE corners
+  [
+    [4, 0], [4, 1], [4, 2], [4, 3], [4, 4],
+    [3, 0], [3, 1], [3, 2], [3, 3], [3, 4],
+    [2, 0], [2, 1], [2, 2], [2, 3], [2, 4],
+    [1, 0], [1, 1], [1, 2], [1, 3], [1, 4],
+    [0, 0], [0, 1], [0, 2], [0, 3], [0, 4]
+  ]
+  """
+  def table(point_x, point_y) when is_integer(point_x) and is_integer(point_y) do
+    data =
+      for x <- point_x..0,
+          y <- point_y..0,
+          do: [x, y]
+
+    Enum.chunk_every(data, 5)
+    |> Enum.map(&Enum.reverse/1)
+    |> Enum.reduce([], fn i, acc -> acc ++ i end)
   end
 end
--- toy_robot/test/toy_robot_test.exs
+++ toy_robot/test/toy_robot_test.exs
@@ -1,8 +1,27 @@
 defmodule ToyRobotTest do
   use ExUnit.Case
-  doctest ToyRobot
 
-  test "greets the world" do
-    assert ToyRobot.hello() == :world
+  # Given a grid
+  # @grid [
+  #   [4, 0], [4, 1], [4, 2], [4, 3], [4, 4],
+  #   [3, 0], [3, 1], [3, 2], [3, 3], [3, 4],
+  #   [2, 0], [2, 1], [2, 2], [2, 3], [2, 4],
+  #   [1, 0], [1, 1], [1, 2], [1, 3], [1, 4],
+  #   [0, 0], [0, 1], [0, 2], [0, 3], [0, 4]
+  # ]
+
+  setup do
+    %{north_w: [4, 0], south_w: [0, 0], north_e: [4, 4], south_e: [0, 4]}
+  end
+
+  describe "Toy Robot table" do
+    test ".table/2", context do
+      assert table = ToyRobot.table(4, 4)
+
+      assert List.first(table) == context.north_w
+      assert Enum.at(table, 4) == context.north_e
+      assert Enum.at(table, 20) == context.south_w
+      assert Enum.at(table, 24) == context.south_e
+    end
   end
 end
