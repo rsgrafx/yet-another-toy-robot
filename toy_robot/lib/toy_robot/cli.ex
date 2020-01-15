668a86746a0f0fe1a47354fed8a42f0541eb1221
Orion Engleton <rsgrafx@gmail.com>
2020-01-15 15:48:39 +1200

    * Mix.exs added escript: option in project/0 [ToyRobot.UI.CLI] - Added module that houses functions that loop over inputs from a CLI   Added some usage documenation in the module.

---
+++ toy_robot/lib/toy_robot/cli.ex
@@ -0,0 +1,121 @@
+defmodule ToyRobot.UI.CLI do
+  @example_usage """
+  ### Available commands
+      PLACE 0,0,NORTH
+      MOVE
+      REPORT
+  """
+
+  @moduledoc """
+    Example Input and Output
+    ------------------------
+    #{@example_usage}
+
+    Expected output:
+        0,1,NORTH
+
+    ### Example b
+
+        PLACE 0,0,NORTH
+        LEFT
+        REPORT
+
+    Expected output:
+
+        0,0,WEST
+
+    ### Example c
+
+        PLACE 1,2,EAST
+        MOVE
+        MOVE
+        LEFT
+        MOVE
+        REPORT
+  """
+
+  def main(data \\ []) do
+    parse_and_process(data)
+  end
+
+  defp parse_and_process(data) do
+    {params, _, _} = OptionParser.parse(data, switches: [help: :boolean])
+    start(params)
+  end
+
+  defp start(help: true) do
+    IO.puts("help messages goes here")
+  end
+
+  defp start(_) do
+    loop_game()
+  end
+
+  defp loop_game(game \\ nil) do
+    gets =
+      IO.gets("> ")
+      |> String.trim()
+      |> String.downcase()
+      |> String.split(" ")
+
+    command(gets, game)
+  end
+
+  defp command(["help" | _], game) do
+    IO.puts("""
+    Available commands
+    > place - # Example usage
+      place 1,1 west #default direction is north
+
+    > help - brings up this prompt
+    > right - turns robot 90 deg right
+    > left - turns robot 90 deg left
+    > move - Robot advances current direction one position
+    """)
+
+    loop_game(game)
+  end
+
+  defp command(["place", params], game) do
+    game = ToyRobot.game(:small, to_coords(params))
+    loop_game(game)
+  end
+
+  defp command(["place", params, facing], game) do
+    game = ToyRobot.game(:small, to_coords(params))
+    loop_game(game)
+  end
+
+  defp command(["left"], game) do
+    game
+    |> ToyRobot.turn_robot(:left)
+    |> loop_game()
+  end
+
+  defp command(["right"], game) do
+    game
+    |> ToyRobot.turn_robot(:right)
+    |> loop_game()
+  end
+
+  defp command(["move"], game) do
+    game
+    |> ToyRobot.move()
+    |> loop_game()
+  end
+
+  defp command(["report"], game) do
+    IO.inspect(game)
+    loop_game(game)
+  end
+
+  defp command(command, game) do
+    IO.inspect(game, label: "unknown: #{command}")
+    loop_game(game)
+  end
+
+  defp to_coords(params) do
+    [x, y] = String.split(params, ",")
+    [String.to_integer(x), String.to_integer(y)]
+  end
+end
--- toy_robot/mix.exs
+++ toy_robot/mix.exs
@@ -7,6 +7,7 @@ defmodule ToyRobot.MixProject do
       version: "0.1.0",
       elixir: "~> 1.9",
       start_permanent: Mix.env() == :prod,
+      escript: [main_module: ToyRobot.UI.CLI],
       deps: deps()
     ]
   end
