b73a8ee0c61d6a4b6175971dd595a71babdb4b01
Orion Engleton <rsgrafx@gmail.com>
2020-01-15 15:46:34 +1200

    Implemented Inspect protocol for ToyRobot.Game - for cli requirements

--- toy_robot/lib/toy_robot/game.ex
+++ toy_robot/lib/toy_robot/game.ex
@@ -5,3 +5,16 @@ defmodule ToyRobot.Game do
     struct(__MODULE__, table: table, robot: robot)
   end
 end
+
+defimpl Inspect, for: ToyRobot.Game do
+  def inspect(nil, _) do
+    "No Game started yet."
+  end
+
+  def inspect(data, _) do
+    """
+    Robot position: #{inspect(data.robot.position)}
+    Facing: #{data.robot.facing}
+    """
+  end
+end
